ff758b1826e1707f6b8aff44fba8c4e1ce19886e
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-06-07 15:54:57 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def main():
             #     code = args[3].strip("User-Agent: ")
             #     response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(code)}\r\n\r\n{code}".encode()
             elif path[1].startswith("/user-agent"):
-                user_agent = args[3].split(": ")[1]
+                user_agent = args[2].split(": ")[1]
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
                 print("Agent:"+user_agent)
             print(f"First par {path}")
