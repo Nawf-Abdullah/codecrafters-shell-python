f0170dc0feda5371fb4a3acf6bd517729eb4c2d0
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-06-07 14:35:46 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -7,7 +7,15 @@ def main():
 
 
     client,addr  = server_socket.accept()
-    client.send("HTTP/1.1 200 OK\r\n\r\n]".encode("utf-8"))
+    data = client.recv(1024)
+    print(addr)
+    decoded_data = data.decode().split("\r\n")
+    print(client)
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    client.send(response)
+    if decoded_data[0].split(" ")[1] != "/":
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    client.sendall(response)
     client.close()
 
 
