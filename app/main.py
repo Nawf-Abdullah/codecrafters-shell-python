e7dd18af974c73cb5b62b244ed687be585738f5b
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-06-08 22:03:39 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@
 import socket
 import threading
 import sys
-
+from io import StringIO 
+import gzip
 
 
 def check_encode(encodings_list):
@@ -18,6 +19,16 @@ def c_handler(conn,addr):
         val = conn.recv(1024)
         pars = val.decode()
         args = pars.split("\r\n")
+        isEncodingAvailable = False
+        for i in args:
+                if i.startswith('Accept-Encoding:') and  check_encode(i.split(':')[1].strip(' ')):
+                    print('Encoding value',i.split(':')[1])
+                    # response = response.decode().split('\r\n')
+                    # response.insert(-2,f'Content-Encoding: gzip',)
+                    # response = '\r\n'.join(response).encode()
+                    isEncodingAvailable = True
+                    break
+        # gzip.compress(bytes(exampleString, 'utf-8'))
         response = b"HTTP/1.1 404 Not Found\r\n\r\n"
         if len(args) > 1:
             path = args[0].split(" ")
@@ -26,8 +37,12 @@ def c_handler(conn,addr):
             if "echo" in path[1]:
                 string = path[1].split("/echo/")
                 print('THE DIVIDED SHIT:',string)
-                
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string[1])}\r\n\r\n{string[1]}".encode()
+                if not isEncodingAvailable:
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string[1])}\r\n\r\n{string[1]}".encode()
+                else:
+                    x = gzip.compress(bytes(string[1], 'utf-8'))
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(x)}\r\n\r\n{x}".encode()
+                    
             # if 'user-agent' in path[1]:
             #     code = args[3].strip("User-Agent: ")
             #     response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(code)}\r\n\r\n{code}".encode()
@@ -52,7 +67,11 @@ def c_handler(conn,addr):
                         x = f.read()
                         print('content',x)
                         content_length = len(x)
-                        response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {content_length}\r\n\r\n{x}".encode()
+                        if not isEncodingAvailable:
+                            response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {content_length}\r\n\r\n{x}".encode()
+                        else:
+                            encodeX =  gzip.compress(bytes(x, 'utf-8'))
+                            response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(encodeX)}\r\n\r\n{encodeX}".encode()
                         print(x)
                 except FileNotFoundError:
                     response = 'HTTP/1.1 404 Not Found\r\n\r\n'.encode()
@@ -66,14 +85,7 @@ def c_handler(conn,addr):
                 
                 #THE ARGS  ['POST /files/file_123 HTTP/1.1', 'Host: localhost:4221', 'User-Agent: curl/8.4.0', 'Accept: */*', 'Content-Type: application/octet-stream', 'Content-Length: 5', '', '12345']
             
-            for i in args:
 
-                if i.startswith('Accept-Encoding:') and  check_encode(i.split(':')[1].strip(' ')):
-                    print('Encoding value',i.split(':')[1])
-                    response = response.decode().split('\r\n')
-                    response.insert(-2,f'Content-Encoding: gzip',)
-                    response = '\r\n'.join(response).encode()
-                    break
 
             print(f"First par {path}")
             
